660ba8d62b29e14a6dcdb03bd97b28c6c9158ade
Шевченко ИГОРЬ Сергеевич (ДАПР) <igorshvch@gmail.com>
2018-05-30 20:21:34 +0300

    first attempt

---
+++ simple.py
@@ -0,0 +1,18 @@
+import math
+import random
+
+MESSAGES = {
+    'init':''
+}
+
+class Population():
+    def __init__(self):
+        pass
+
+class Mutations():
+    def __init__(self):
+        pass
+        
+class Environment():
+    def __init__(self):
+        pass
